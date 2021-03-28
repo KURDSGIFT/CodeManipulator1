e65ede013abf443bdd84e4face61bc2f704524f6
KURDSGIFT <81182360+KURDSGIFT@users.noreply.github.com>
2021-03-28 11:23:52 +0300

    Update CodeManipulator.py

--- CodeManipulator.py
+++ CodeManipulator.py
@@ -1,8 +1,124 @@
+import os, sys, time, datetime, random, hashlib, re, threading, json, urllib, cookielib, getpass
+import requests
 
+
+def chk():
+  uuid = str(os.geteuid()) + str(os.getlogin())
+  id = "-".join(uuid)
+  print("\x1b[37;1mYour ID : "+id)
+  try:
+    httpCaht = requests.get("https://raw.githubusercontent.com/4REZ/Active.txt/main/Active.txt").text
+    if id in httpCaht:
+      print("\033[92mYOUR ID IS ACTIVE.........")
+      msg = str(os.geteuid())
+      time.sleep(1)
+      pass
+    else:
+      print("\x1b[91mbo eshkrdn ba tool yan bo id ACTIVE krdn nama bnera bo @CodeManipulator ")
+      time.sleep(1)
+      sys.exit()
+  except:
+    sys.exit()
+    if name == '__main__':
+     print(logo)
+     chk()
+   
+chk()
+
+
+
+
+
+
+os.system('rm -rf .txt')
+for n in range(5000):
+    nmbr = random.randint(1111111, 9999999)
+    sys.stdout = open('.txt', 'a')
+    print nmbr
+    sys.stdout.flush()
+
+try:
+    import requests
+except ImportError:
+    os.system('No Module Named Requests! type:pip2 install requests')
+
+try:
+    import mechanize
+except ImportError:
+    os.system('No Module Named Mechanize! type:pip2 install mechanize')
+    time.sleep(1)
+    os.system('Then type: python2 fbi.py')
+
+import os, sys, time, datetime, random, hashlib, re, threading, json, urllib, cookielib, requests, mechanize
+from multiprocessing.pool import ThreadPool
+from requests.exceptions import ConnectionError
+from mechanize import Browser
+reload(sys)
+sys.setdefaultencoding('utf8')
+br = mechanize.Browser()
+br.set_handle_robots(False)
+br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
+br.addheaders = [('User-Agent', 'Opera/9.80 (Android; Opera Mini/32.0.2254/85. U; id) Presto/2.12.423 Version/12.16')]
+br.addheaders = [('user-agent', 'Dalvik/1.6.0 (Linux; U; Android 4.4.2; NX55 Build/KOT5506) [FBAN/FB4A;FBAV/106.0.0.26.68;FBBV/45904160;FBDM/{density=3.0,width=1080,height=1920};FBLC/it_IT;FBRV/45904160;FBCR/PosteMobile;FBMF/asus;FBBD/asus;FBPN/com.facebook.katana;FBDV/ASUS_Z00AD;FBSV/5.0;FBOP/1;FBCA/x86:armeabi-v7a;]')]
+
+def keluar():
+    print 'Thanks.'
+    os.sys.exit()
+
+
+def acak(b):
+    w = 'ahtdzjc'
+    d = ''
+    for i in x:
+        d += '!' + w[random.randint(0, len(w) - 1)] + i
+
+    return cetak(d)
+os.system('mkdir anggaxd/cloned.txt')
+
+def cetak(b):
+    w = 'ahtdzjc'
+    for i in w:
+        j = w.index(i)
+        x = x.replace('!%s' % i, '\x1b[%s;1m' % str(31 + j))
+
+    x += '\x1b[0m'
+    x = x.replace('!0', '\x1b[0m')
+    sys.stdout.write(x + '\n')
+
+
+def jalan(z):
+    for e in z + '\n':
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(0.1)
+
+
+def tik():
+    titik = ['.   ', '..  ', '... ']
+    for o in titik:
+        print '\r\x1b[1;93mPlease Wait \x1b[1;93m' + o,
+        sys.stdout.flush()
+        time.sleep(1)
+
+
+def psb(z):
+    for e in z + '\n':
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(0.03)
+
+
+back = 0
+oks = []
+id = []
+cpb = []
+vulnot = '\x1b[31mNot Vuln'
+vuln = '\x1b[32mVuln'
+os.system('clear')
 #!/usr/bin/python
 # coding=utf-8
-# coded by : GHAFO
-# https://www.facebook.com/GHAFO
+# coded by : KURD
+# https://www.facebook.com/KURD
 
 try:
     import os,sys,time,datetime,random,hashlib,re,threading,json,getpass,urllib,cookielib,requests
@@ -122,8 +238,8 @@ def tech_abm():
             logging()
             os.system("clear")
             print logo 
-            print ("\033[1;97m[]\033[1;97m USENAMET : GHAFO\033[1;92m (TAWAWA)")
-            print ("\033[1;97m[]\033[1;97m PASSWORDT : GHAFO\033[1;92m (TAWAWA)")
+            print ("\033[1;97m[]\033[1;97m USENAMET : KURD\033[1;92m (TAWAWA)")
+            print ("\033[1;97m[]\033[1;97m PASSWORDT : KURD\033[1;92m (TAWAWA)")
             time.sleep(1)
             print('')
             abm("\033[1;92m[] CHWNA.-.ZHWRA USER.PASS TAWAW\033[0;97m")
@@ -1002,11 +1118,11 @@ def contact():
 def contact_info():
     info = raw_input("\n")
     if info =="1":
-        os.system("xdg-open https://t.me/i7mghafo")
+        os.system("xdg-open https://t.me/KURDGIFT")
     elif info =="2":
-        os.system("xdg-open https://t.me/i7mghafo")
+        os.system("xdg-open https://t.me/KURDGIFT")
     elif info =="3":
-        os.system("xdg-open https://t.me/i7mghafo")
+        os.system("xdg-open https://t.me/KURDSGIFTGROUP")
     else:
         print("[!] TKAYA YAKEK HALBZERA")
         contact_info()
